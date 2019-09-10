52cde5b89c4edc614eeead51e82869d3db77b66e
breaker33 <1152211980@qq.com>
2019-09-10 21:40:51 +0800

    first commit

---
+++ src/main/java/com/community/suzn/SuznApplication.java
@@ -0,0 +1,13 @@
+package com.community.suzn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuznApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuznApplication.class, args);
+    }
+
+}
